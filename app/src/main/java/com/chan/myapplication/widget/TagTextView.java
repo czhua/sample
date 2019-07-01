3d20b80b6773922bbd696cdb82ab2911cae80069
chan <zehua.ch@gmail.com>
2019-07-01 15:15:39 +0800

    处理显示异常问题

--- app/src/main/java/com/chan/myapplication/widget/TagTextView.java
+++ app/src/main/java/com/chan/myapplication/widget/TagTextView.java
@@ -2,44 +2,61 @@ package com.chan.myapplication.widget;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewTreeObserver;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import androidx.annotation.ColorInt;
 
 /**
  * 带便签的TextView
+ *
+ * @author chan
  */
 public class TagTextView extends RelativeLayout {
 
     private static final String TAG = "TagTextView";
 
-    private String mText;
-    private String mTagText;
+    /**
+     * 默认开始遍历的下标，用于减少遍历次数，一定要确保使用当前字符数量没有超过一行，否则会显示异常
+     */
+    private static final int DEFAULT_START_POSITION = 10;
 
+    /**
+     * 文字默认大小
+     */
+    private static final int DEFAULT_TEXT_SIZE = 12;
+    /**
+     * tag水平方向的padding
+     */
+    private static final int DEFAULT_TAG_PADDING_HOR = 6;
+    /**
+     * tag垂直方向的padding
+     */
+    private static final int DEFAULT_TAG_PADDING_VER = 1;
+    /**
+     * 文字默认颜色
+     */
+    private static final int DEFAULT_TEXT_COLOR = Color.WHITE;
+    /**
+     * 标签默认大小
+     */
+    private static final int DEFAULT_TAG_TEXT_SIZE = 10;
     /**
      * 标签的leftMargin
      */
     private static final int DEFAULT_TAG_MARGIN_LEFT = 4;
     /**
-     * 文字默认大小
-     */
-    private static final int DEFAULT_TEXT_SIZE = 10;
-    /**
-     * 文字默认颜色
-     */
-    private static final int DEFAULT_TEXT_COLOR = Color.BLACK;
-    /**
-     * 标签默认大小
-     */
-    private static final int DEFAULT_TAG_TEXT_SIZE = DEFAULT_TEXT_SIZE;
-    /**
      * 标签默认颜色
      */
-    private static final int DEFAULT_TAG_TEXT_COLOR = Color.RED;
+    private static final int DEFAULT_TAG_TEXT_COLOR = Color.WHITE;
+    private static final int DEFAULT_TAG_BG_RADIUS = 3;
+    private static final String DEFAULT_TAG_BG_COLOR = "#66939393";
 
     private static final int ID_FIRST = 1001;
     private static final int ID_END = 1002;
@@ -47,6 +64,11 @@ public class TagTextView extends RelativeLayout {
     private TextView mFirstTv;
     private TextView mEndTv;
     private TextView mTagTv;
+    private int mHorPadding = DEFAULT_TAG_PADDING_HOR;
+    private int mVerPadding = DEFAULT_TAG_PADDING_VER;
+    private String mText;
+    private String mTagText;
+    private GradientDrawable mTagBgDrawable;
 
     public TagTextView(Context context) {
         this(context, null);
@@ -65,66 +87,97 @@ public class TagTextView extends RelativeLayout {
         mText = text;
         mTagText = tag;
         mTagTv.setText(tag);
-
-//        mTagTv.post(new Runnable() {
-//            @Override
-//            public void run() {
-//                updateView(text);
-//            }
-//        });
-
-        getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
-            @Override
-            public void onGlobalLayout() {
-                getViewTreeObserver().removeOnGlobalLayoutListener(this);
-                updateView(text, tag);
-            }
-        });
-
     }
 
-    private void updateView(String text, String tag) {
+    /**
+     * 设置文本颜色
+     *
+     * @param color color
+     */
+    public void setTextColor(@ColorInt int color) {
+        mFirstTv.setTextColor(color);
+        mEndTv.setTextColor(color);
+        mTagTv.setTextColor(color);
+    }
+
+    public void setTagBackgroundColor(@ColorInt int color) {
+        mTagBgDrawable.setColor(color);
+        mTagTv.setBackground(mTagBgDrawable);
+    }
+
+    /**
+     * 设置tag的水平padding
+     *
+     * @param padding 水平padding， dp
+     */
+    public void setPaddingHor(int padding) {
+        mHorPadding = DensityUtil.dip2px(getContext(), padding);
+    }
+
+    private void updateView() {
+        if (TextUtils.isEmpty(mText) || TextUtils.isEmpty(mTagText)) {
+            Log.d(TAG, "text is empty text:" + mText + ", tag text:" + mTagText);
+            return;
+        }
         int width = getMeasuredWidth();
-        float tagWidth = mTagTv.getPaint().measureText(tag);
+        float tagWidth = mTagTv.getPaint().measureText(mTagText) + 2 * mHorPadding;
         Log.d(TAG, "width:" + width + ", tag width:" + tagWidth);
-        String[] arr = text.split("");
+        String[] arr = mText.split("");
         StringBuilder buffer = new StringBuilder();
-        label:
-        for (int i = 0; i < arr.length; i++) {
-            buffer.append(arr[i]);
-            float textWidth = mFirstTv.getPaint().measureText(buffer.toString());
-            if (textWidth > width) {
-                Log.d(TAG, "buffer: " + buffer);
-                buffer.deleteCharAt(buffer.length() - 1);
-                char end;
-                StringBuilder endBuffer = new StringBuilder();
-                for (int index = buffer.length() - 1; index >= 0; index--) {
-                    end = buffer.charAt(index);
-                    if (Character.isLowerCase(end) || Character.isUpperCase(end)) {
-                        endBuffer.append(end);
-                    } else {
-                        Log.d(TAG, "length: " + buffer.length() + " end len:" + endBuffer.length());
-                        Log.d(TAG, "buffer:" + buffer + ", end:" + endBuffer);
-                        buffer.delete(buffer.length() - endBuffer.length(), buffer.length());
-                        break label;
+        float firstWidth = 0;
+        if (arr.length > DEFAULT_START_POSITION) {
+            buffer.append(mText.substring(0, DEFAULT_START_POSITION - 1));
+            label:
+            for (int i = DEFAULT_START_POSITION; i < arr.length; i++) {
+                buffer.append(arr[i]);
+                firstWidth = mFirstTv.getPaint().measureText(buffer.toString());
+                if (firstWidth > width) {
+                    firstWidth = width;
+                    Log.d(TAG, "buffer: " + buffer);
+                    buffer.deleteCharAt(buffer.length() - 1);
+                    char end;
+                    StringBuilder endBuffer = new StringBuilder();
+                    for (int index = buffer.length() - 1; index >= 0; index--) {
+                        end = buffer.charAt(index);
+                        if (Character.isLowerCase(end) || Character.isUpperCase(end)) {
+                            endBuffer.append(end);
+                            if (index <= 0) {
+                                // 当前单词超过整行
+                                endBuffer.setLength(0);
+                                break label;
+                            }
+                            Log.d(TAG, "append end:" + end + ", end buffer:" + endBuffer);
+                        } else {
+                            Log.d(TAG, "length: " + buffer.length() + " end len:" + endBuffer.length());
+                            Log.d(TAG, "buffer:" + buffer + ", end:" + endBuffer);
+                            if (endBuffer.length() > 0) {
+                                buffer.delete(buffer.length() - endBuffer.length(), buffer.length());
+                            }
+                            break label;
+                        }
                     }
                 }
             }
         }
 
         Log.d(TAG, "end buffer:" + buffer);
-        float firstLen = mFirstTv.getPaint().measureText(buffer.toString());
         float maxWidth = width - tagWidth - DensityUtil.dip2px(getContext(), DEFAULT_TAG_MARGIN_LEFT);
-        if (firstLen <= maxWidth) {
-            Log.d(TAG, "single line first len:" + firstLen + ", max width:" + maxWidth);
+        if (firstWidth <= maxWidth) {
+            Log.d(TAG, "single line first width:" + firstWidth + ", max width:" + maxWidth);
             mFirstTv.setText(buffer);
             mEndTv.setVisibility(View.GONE);
         } else {
             mFirstTv.setText(buffer);
             mEndTv.setVisibility(View.VISIBLE);
-            mEndTv.setText(text.substring(buffer.length()).trim());
+            String endText = mText.substring(buffer.length()).trim();
+            mEndTv.setText(endText);
             mEndTv.setMaxWidth((int) maxWidth);
             LayoutParams lp = (LayoutParams) mTagTv.getLayoutParams();
+            if (TextUtils.isEmpty(endText)) {
+                lp.setMarginStart(0);
+            } else {
+                lp.setMarginStart(DensityUtil.dip2px(getContext(), DEFAULT_TAG_MARGIN_LEFT));
+            }
             lp.addRule(ALIGN_BASELINE, ID_END);
             lp.addRule(END_OF, ID_END);
             mTagTv.setLayoutParams(lp);
@@ -132,6 +185,13 @@ public class TagTextView extends RelativeLayout {
     }
 
     private void init(Context ctx) {
+        mHorPadding = DensityUtil.dip2px(ctx, DEFAULT_TAG_PADDING_HOR);
+        mVerPadding = DensityUtil.dip2px(ctx, DEFAULT_TAG_PADDING_VER);
+
+        mTagBgDrawable = new GradientDrawable();
+        mTagBgDrawable.setCornerRadius(DensityUtil.dip2px(getContext(), DEFAULT_TAG_BG_RADIUS));
+        mTagBgDrawable.setColor(Color.parseColor(DEFAULT_TAG_BG_COLOR));
+
         mFirstTv = initFirstView(ctx);
         mEndTv = initEndView(ctx);
         mTagTv = initTagView(ctx);
@@ -139,13 +199,19 @@ public class TagTextView extends RelativeLayout {
         addView(mEndTv);
         addView(mTagTv);
 
-
+        getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                updateView();
+            }
+        });
     }
 
     private TextView initFirstView(Context ctx) {
         TextView view = new TextView(ctx);
         view.setId(ID_FIRST);
-        view.setTextSize(DensityUtil.dip2px(ctx, DEFAULT_TEXT_SIZE));
+        view.setTextSize(TypedValue.COMPLEX_UNIT_DIP, DEFAULT_TEXT_SIZE);
         view.setTextColor(DEFAULT_TEXT_COLOR);
         view.setMaxLines(1);
         view.setEllipsize(TextUtils.TruncateAt.END);
@@ -155,7 +221,7 @@ public class TagTextView extends RelativeLayout {
     private TextView initEndView(Context ctx) {
         TextView view = new TextView(ctx);
         view.setId(ID_END);
-        view.setTextSize(DensityUtil.dip2px(ctx, DEFAULT_TEXT_SIZE));
+        view.setTextSize(TypedValue.COMPLEX_UNIT_DIP, DEFAULT_TEXT_SIZE);
         view.setTextColor(DEFAULT_TEXT_COLOR);
         view.setMaxLines(1);
         view.setEllipsize(TextUtils.TruncateAt.END);
@@ -168,11 +234,14 @@ public class TagTextView extends RelativeLayout {
 
     private TextView initTagView(Context ctx) {
         TextView view = new TextView(ctx);
-        view.setTextSize(DensityUtil.dip2px(ctx, DEFAULT_TAG_TEXT_SIZE));
+        view.setMaxLines(1);
+        view.setTextSize(TypedValue.COMPLEX_UNIT_DIP, DEFAULT_TAG_TEXT_SIZE);
+        view.setBackground(mTagBgDrawable);
+        view.setPadding(mHorPadding, mVerPadding, mHorPadding, mVerPadding);
         view.setTextColor(DEFAULT_TAG_TEXT_COLOR);
         LayoutParams tagLp = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-//        tagLp.addRule(BELOW, ID_FIRST);
         tagLp.addRule(ALIGN_BASELINE, ID_FIRST);
+        tagLp.setMarginStart(DensityUtil.dip2px(ctx, DEFAULT_TAG_MARGIN_LEFT));
         tagLp.addRule(END_OF, ID_FIRST);
         view.setLayoutParams(tagLp);
         return view;
